67841fcb4d40db2c8442bcb7a913f68b763be782
Electra Chong <electrachong@gmail.com>
2016-05-31 23:37:29 +0000

    Create another workaround because the array could not be parsed into a unicode string

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
+# encoding: utf-8
+
 from flask import Flask, render_template, json, request
 import fileinput
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,27 +22,15 @@ def create_entry():
     homepage = open("templates/index.html", "r")                                                           
     contents = homepage.readlines()                                                                            
     homepage.close() 
-    
-    entry_hash = return_entry(_title, contents)
-
-    '''homepage = open("test_file", 'a')
-    homepage.write("<!--" + entry_hash["entry"] + "-->")
-    homepage.close'''
-    
-    homepage = open("test_file", 'a')
-    homepage.write("<!--" + entry_hash["entry"] + "-->")
+        
+    homepage = open("test_file", 'w')
+    for line in contents:
+        homepage.write(line)
+        if '<table border="1">' in line:
+            homepage.write('<tr class="entry_cell"><td class="img_cells"><a href="sample.txt"\><img src="http://img.pokemondb.net/sprites/black-white/normal/abra.png" alt="Abra"></a></td><td class="title"><a href="sample.txt">' + _title + '</a></td><td class="timestamp">[timestamp]</td></tr>')
     homepage.close
 
     return json.dumps({'html':'<span>All fields good!</span>'})
 
 if __name__ == "__main__":
     app.run()
-
-def return_entry(title, contents):
-    i = 1
-    for line in contents:
-        if '<table border="1">' in line:
-            line = line.replace(line, line + '<tr class="entry_cell"><td class="img_cells"><a href="sample.txt"\><img src="http://img.pokemondb.net/sprites/black-white/normal/abra.png" alt="Abra"></a></td><td class="title"><a href="sample.txt">' + title + '</a></td><td class="timestamp">[timestamp]</td></tr>')
-            return {'line': i, 'entry': line}
-        i+=1
-    return None
