2b387bbed124ee43d550c6528c5aff973cb86d10
Electra Chong <electrachong@gmail.com>
2016-05-31 22:29:44 +0000

    Modify app.py so that we try to append a new entry in the homepage

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, json, request
+import fileinput
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,11 +16,25 @@ def create_entry():
     _filetype = request.form['filetype']
     _body = request.form['bodytext']
 
-    homepage = open("templates/index.html", 'a')
-    homepage.write("<!--" + _title + "-->")
+    entry = return_entry(_title)
+    print entry
+    
+    ''' homepage = open("templates/index.html", "r")
+    contents = homepage.readlines()
+    homepage.close() '''
+
+    homepage = open("test_file", 'a')
+    homepage.write("<!--" + entry + "-->")
     homepage.close
     
     return json.dumps({'html':'<span>All fields good!</span>'})
 
 if __name__ == "__main__":
     app.run()
+
+def return_entry(title):
+    for line in fileinput.FileInput("templates/index.html",inplace = 1):
+        if '<table border="1">' in line:
+            line = line.replace(line, line + '<tr class="entry_cell"><td class="img_cells"><a href="sample.txt"><img src="http://img.pokemondb.net/sprites/black-white/normal/abra.png" alt="Abra"></a></td><td class="title"><a href="sample.txt">' + _title + '</a></td><td class="timestamp">[timestamp]</td></tr>')
+            return line
+    return "testing4"
